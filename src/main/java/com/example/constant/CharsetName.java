49686cc28d22cbfe706a9d2bffe5bc0a29c36098
chengdongyi <13088805160@163.com>
2019-12-02 14:38:53 +0800

    字符集编码

---
+++ src/main/java/com/example/constant/CharsetName.java
@@ -0,0 +1,17 @@
+package com.example.constant;
+
+/**
+ * @description: 常用字符集编码
+ * @author chengdongyi
+ * @date 2019/12/2 11:11
+ */
+public class CharsetName {
+
+    public static final String UTF_8 = "UTF-8";
+    public static final String UTF_16 = "UTF-16";
+    public static final String ASCII = "ASCII";
+    public static final String US_ASCII = "US-ASCII";
+    public static final String ISO_8859_1 = "ISO-8859-1";
+    public static final String GBK = "GBK";
+
+}
