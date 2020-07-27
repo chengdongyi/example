33517720f32966e979bf245f37f35f29c0c45843
chengdongyi <13088805160@163.com>
2020-07-27 15:52:19 +0800

    AES对称加密

--- src/main/java/com/example/util/AESUtil.java
+++ src/main/java/com/example/util/AESUtil.java
@@ -1,8 +1,11 @@
 package com.example.util;
 
-import javax.crypto.Cipher;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 /**
  * @description: AES对称加密
@@ -12,46 +15,157 @@ import java.nio.charset.StandardCharsets;
 public class AESUtil {
 
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(encryptAES( "12345678","123"));
+    /**
+     * 功能描述: 初始化 AES Cipher
+     * @author chengdongyi
+     * @date 2020/5/19 13:06
+     * @param key 秘钥
+     * @param cipherMode
+     * @return javax.crypto.Cipher
+     */
+    public static Cipher initAESCipher(String key, int cipherMode) {
+        //创建Key gen
+        KeyGenerator keyGenerator = null;
+        Cipher cipher = null;
+        try {
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom.setSeed(key.getBytes());
+            keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128, secureRandom);
+            SecretKey secretKey = keyGenerator.generateKey();
+            byte[] codeFormat = secretKey.getEncoded();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(codeFormat, "AES");
+            cipher = Cipher.getInstance("AES");
+            //初始化
+            cipher.init(cipherMode, secretKeySpec);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return cipher;
     }
 
-        /**
-         * 加密
-         *
-         * @param content
-         * @param key
-         * @return
-         */
-        public static String encryptAES(String content, String key) {
+
+    /**
+     * 功能描述: 对文件进行 AES 加密
+     * @author chengdongyi
+     * @date 2020/5/19 11:15
+     * @param sourceFile 源文件
+     * @param path 加密后生成的文件路径
+     * @param suffix 机密后的文件后缀
+     * @param key 秘钥
+     * @return java.io.File
+     */
+    public static File encryptFile(File sourceFile, String path, String suffix, String key, String logUniqueFlag) {
+
+        File encrypFile = null;
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new FileInputStream(sourceFile);
+            String encrypFileName = sourceFile.getName() + suffix;
+            encrypFile = new File(path + encrypFileName);
+            outputStream = new FileOutputStream(encrypFile);
+            Cipher cipher = initAESCipher(key, Cipher.ENCRYPT_MODE);
+            //以加密流写入文件
+            CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher);
+            byte[] cache = new byte[1024];
+            int nRead = 0;
+            while ((nRead = cipherInputStream.read(cache)) != -1) {
+                outputStream.write(cache, 0, nRead);
+                outputStream.flush();
+            }
+            cipherInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
             try {
-                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
-                // "算法/模式/补码方式"
-                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-                cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-                byte[] result = cipher.doFinal(content.getBytes(StandardCharsets.UTF_8));
-                return parseByte2HexStr(result);
-            } catch (Exception e) {
-//                log.error(String.format("加密失败:，content：%s, key: %s", content, key));
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return content;
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        /**
-         * 将二进制转换成16进制
-         *
-         * @param buf
-         * @return
-         */
-        public static String parseByte2HexStr(byte[] buf) {
-            StringBuilder sb = new StringBuilder();
-            for (byte b : buf) {
-                String hex = Integer.toHexString(b & 0xFF);
-                if (hex.length() == 1) {
-                    hex = '0' + hex;
+        return encrypFile;
+    }
+
+
+    /**
+     * 功能描述: AES方式解密文件
+     * @author chengdongyi
+     * @date 2020/5/19 13:05
+     * @param sourceFile 源文件（待解密的文件）
+     * @param path 解密后的文件路径
+     * @param suffix 需要去掉的文件后缀
+     * @param key 秘钥
+     * @return java.io.File
+     */
+    public static File decryptFile(File sourceFile, String path, String suffix, String key) {
+        File decryptFile = null;
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            String decryptFileame = null;
+            if (sourceFile.getName().endsWith(suffix)) {
+                decryptFileame = sourceFile.getName().substring(0, sourceFile.getName().lastIndexOf(suffix));
+            }
+            decryptFile = new File(path + decryptFileame);
+            Cipher cipher = initAESCipher(key, Cipher.DECRYPT_MODE);
+            inputStream = new FileInputStream(sourceFile);
+            outputStream = new FileOutputStream(decryptFile);
+            CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, cipher);
+            byte[] buffer = new byte[1024];
+            int r;
+            while ((r = inputStream.read(buffer)) >= 0) {
+                cipherOutputStream.write(buffer, 0, r);
+            }
+            cipherOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
                 }
-                sb.append(hex.toUpperCase());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return sb.toString();
+            try {
+                if (outputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return decryptFile;
+    }
+
+    public static void main(String[] args) {
+
+        // 秘钥
+        String key = "a0f79a9714d63f505a6e8fb3aef2e073";
+
+        String path = "D:\\test\\iop\\20200509\\";
+        // 文件后缀
+        String suffix = ".encrypt";
+
+        // 加密
+        File file = new File(path + "a_10000_20200608_JZYY-RWK-50101_00.verf");
+        encryptFile(file, path, suffix, key, "");
+
+        // 解密
+//        File file = new File(path + "a_10000_20200608_JZYY-RWK-50101_00.verf.encrypt");
+//        decryptFile(file, path, suffix, key);
+
+    }
 
 }
